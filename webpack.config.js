26167a994244607f2e9efa6d1a303ff898676abd
michaeltp93 <michael.trejosp@hotmail.es>
2021-07-01 14:05:37 +0200

    fixed some problems with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
 		rules: [
 			{
 				test: /\.tsx?$/,
-				exclude: /node_modules/,
+				exclude: /node_module/,
 				use: ['babel-loader', 'stylelint-custom-processor-loader'],
 			},
 		],
